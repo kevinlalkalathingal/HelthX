98bf004863ab7f8fc0c34a6533eeb81e486b580b
Kevin Thomas Lal <kevinlal8605@gmail.com>
2021-04-16 00:41:15 +0530

    HelthX 1.2.1

--- app/src/main/java/com/example/helth/MainActivity.java
+++ app/src/main/java/com/example/helth/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.helth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.internal.InternalTokenProvider;
@@ -29,7 +33,30 @@ public class MainActivity extends AppCompatActivity {
                 logout();
             }
         });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemReselectedListener(onNav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new Fragment2()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemReselectedListener onNav = new BottomNavigationView.OnNavigationItemReselectedListener() {
+        @Override
+        public void onNavigationItemReselected(@NonNull MenuItem item) {
+
+            Fragment selected = null;
+            switch (item.getItemId()){
+                case R.id.home_button:
+                    selected = new Fragment1();
+                    break;
+
+                case R.id.profile_button:
+                    selected = new Fragment2();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, selected).commit();
+        }
+    };
 
     public void logout() {
         FirebaseAuth.getInstance().signOut();
